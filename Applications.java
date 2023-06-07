4646d2d6c7155d79eaaa6249d37871a031d05f75
HitnalliSarvesh <sarveshhitnalli.xworkz@gmail.com>
2023-06-07 13:36:56 +0530

    Uploading file

---
+++ Applications.java
@@ -0,0 +1,20 @@
+class Applications{
+public static void main(String a[]){
+byte third=12;
+System.out.println("Third party applications:"+third);
+short finance=6;
+System.out.println("Finance realted applications:"+finance);
+int edit=205;
+System.out.println("Photo editing applications:"+edit);
+long video=5050;
+System.out.println("Video editing applications:"+video);
+float games=250;
+System.out.println("Games applications:"+games);
+double total=53909;
+System.out.println("Total no. of applications:"+total);
+boolean good=true;
+System.out.println("All applications are good:"+good);
+char ch='G';
+System.out.println("Google:"+ch);
+}
+}
