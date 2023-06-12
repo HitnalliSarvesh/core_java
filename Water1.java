0090668990a465f85fd8dafe2f3fa8b6d5c17842
HitnalliSarvesh <sarveshhitnalli.xworkz@gmail.com>
2023-06-12 10:46:59 +0530

    Uploaded

---
+++ Water1.java
@@ -0,0 +1,5 @@
+class Water1{
+public Water1(){
+
+}
+}
---
+++ Water1Runner.java
@@ -0,0 +1,19 @@
+class Water1Runner{
+public static void main(String args[]){
+	Water1 bisleriWater= new
+    Water1();	
+	System.out.println("First instance:"+bisleriWater);
+	Water1 tapWater= new
+	Water1();
+	System.out.println("Second instance:"+tapWater);
+	Water1 drinkingWater= new
+	Water1();
+	System.out.println("Third instance:"+drinkingWater);
+	Water1 tankWater= new
+	Water1();
+	System.out.println("Fourth instance:"+tankWater);
+	Water1 blackWater= new
+	Water1();
+	System.out.println("Fifth instance:"+blackWater);
+}
+}
